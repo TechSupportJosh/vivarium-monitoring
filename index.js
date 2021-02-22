b7b7ce85ec0a181da8d913a7cda7cd0e39896ece
Joshua Hawking <joshuahawking1@gmail.com>
2021-02-22 00:30:15 +0000

    Added functionality to /data to upload/receive data.

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const safeCompare = require('safe-compare');
-
-dotenv.config();
+const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
+// Load environment vars
+dotenv.config();
+
 app.use(express.json());
 
-const authMiddleware = (req, res, next) => {
-  const providedAuth = req.headers["X-API-KEY"] ?? "";
+const db = new sqlite3.Database(':memory:');
 
+// Initialise database
+db.serialize(function() {
+    db.run(`CREATE TABLE IF NOT EXISTS sensor_data (
+                id INTEGER PRIMARY KEY, 
+                sensor VARCHAR(20), 
+                temperature DOUBLE, 
+                humidity INTEGER
+            )`);
+    
+    // load dummy data
+    for (var i = 0; i < 0; i++) {
+        db.run("INSERT INTO sensor_data (sensor, temperature, humidity) VALUES ($sensor, $temperature, $humidity)", {
+            $sensor: "cool", 
+            $temperature: 20 + Math.random()*30,
+            $humidity: Math.floor(Math.random()*100)
+        });
+    }
+  });
+  
+
+const authMiddleware = (req, res, next) => {
+  const providedAuth = req.headers["x-api-key"] ?? "";
+  
   if (!safeCompare(providedAuth, process.env.API_KEY)) {
     return res.sendStatus(401);
   }
@@ -19,13 +43,38 @@ const authMiddleware = (req, res, next) => {
 };
 
 app.get("/data", (req, res) => {
-  console.log("GET /data");
-  return res.sendStatus(200);
+  // TODO: Add cache
+  db.all("SELECT * FROM sensor_data", (err, rows) => {
+    if(err)
+      return res.sendStatus(500);
+
+    return res.json(rows);
+  });
 });
 
 app.post("/data", authMiddleware, (req, res) => {
-  console.log("POST /data", req.body);
-  return res.sendStatus(201);
+  const {sensor, temperature, humidity} = req.body;
+  // All requests must include a sensor name, temperature and humidity
+  if(!sensor || !temperature || !humidity)
+    return res.sendStatus(400);
+
+  if(typeof sensor !== "string" || typeof temperature !== "number" || typeof humidity !== "number")
+    return res.sendStatus(400);
+
+  db.run("INSERT INTO sensor_data (sensor, temperature, humidity) VALUES ($sensor, $temperature, $humidity)", {
+    $sensor: sensor,
+    $temperature: temperature,
+    $humidity: humidity
+  }, (err) => {
+    if(err)
+    {
+      console.error("Error when inserting data into database", err);
+      return res.sendStatus(500);
+    }
+
+    console.log(`Received datapoint from ${sensor}: ${temperature}°C, ${humidity}%`);
+    return res.sendStatus(201);
+  });
 });
 
 app.listen(9000, () => {
